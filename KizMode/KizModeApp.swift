b0a8ae9c79ba4283acb32da44817cfb22a2d54cf
Hyeon Sang Lee <250wons@gmail.com>
2022-05-05 13:12:39 +0900

    Initial Commit

---
+++ KizMode/KizModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KizModeApp.swift
+//  KizMode
+//
+//  Created by Hyeon-sang Lee on 2022/05/05.
+//
+
+import SwiftUI
+
+@main
+struct KizModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
